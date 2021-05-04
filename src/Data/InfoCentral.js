c5d967b6322f252aed7e85d16b7a24dd1f1a644c
Shivam Singh <57267960+1502shivam-singh@users.noreply.github.com>
2021-05-04 19:52:26 +0530

    Rearranged objects for district preference

--- src/Data/InfoCentral.js
+++ src/Data/InfoCentral.js
@@ -30,6 +30,78 @@ const centralData = [
     tags: ["agra", "uttar pradesh", "uttarpradesh", "uttar-pradesh"]
   },
   {
+    title: "Covid-19 - Pune",
+    link: "https://covidpune.com/",
+    img: "/assets/images/hospital.png",
+    tags: ["pune", "maharashtra"]
+  },
+  {
+    title: "Covid-19 - Beed",
+    link: "https://covidbeed.com/",
+    img: "/assets/images/hospital.png",
+    tags: ["beed", "maharashtra"]
+  },
+  {
+    title: "Covid-19 Resources - Hyderabad",
+    link: "https://hydcovidresources.com/",
+    img: "/assets/images/hospital.png",
+    tags: ["hyd","hyderabad", "telangana"]
+  },
+  {
+    title: "Covid-19 - Gandhinagar",
+    link: "https://covidgandhinagar.com/",
+    img: "/assets/images/hospital.png",
+    tags: ["gandhinagar", "gandhi nagar", "gandhi-nagar", "gujarat", "guj"]
+  },
+  {
+    title: "Covid-19 - Jamshedpur",
+    link: "https://jamshedpurguru.in/coveasy/",
+    img: "/assets/images/hospital.png",
+    tags: ["jamshedpur", "jamshed pur", "jamshed-pur", "jamshed", "jharkhand"]
+  },
+  {
+    title: "Covid-19 - Nashik",
+    link: "https://covidnashik.com/",
+    img: "/assets/images/hospital.png",
+    tags: ["nashik", "nasik", "maharashtra"]
+  },
+  {
+    title: "Covid-19 - TamilNadu",
+    link: "https://covidtnadu.com/",
+    img: "/assets/images/hospital.png",
+    tags: ["tamilnadu", "tamil nadu", "tamil-nadu", "tamil", "tn"]
+  },
+  {
+    title: "Stop corona - TamilNadu",
+    link: "https://stopcorona.tn.gov.in/",
+    img: "/assets/images/hospital.png",
+    tags: ["tamilnadu", "tamil nadu", "tamil-nadu", "tn", "tamil"]
+  },
+  {
+    title: "Covidbangalore.support (Instagram)",
+    link: "https://www.instagram.com/covidbangalore.support/",
+    img: "/assets/images/hospital.png",
+    tags: ["karnataka", "karnatak", "bangalore", "bengaluru"]
+  },
+  {
+    title: "Covid-19 resources - Bangalore",
+    link: "https://oxygenblr.in/",
+    img: "/assets/images/hospital.png",
+    tags: ["karnataka", "karnatak", "bangalore", "bengaluru"]
+  },
+  {
+    title: "Covid-19 resources - Kolkata",
+    link: "https://docs.google.com/spreadsheets/u/0/d/1dy1DYjGb15bijab4vVAUyrdtw14va32NgWJqlo_2rcU/htmlview",
+    img: "/assets/images/hospital.png",
+    tags: ["west bengal", "west-bengal", "westbengal", "bengal", "kolkata", "calcutta"]
+  },
+  {
+    title: "Pune Covid - links",
+    link: "https://linktr.ee/punecovid",
+    img: "/assets/images/hospital.png",
+    tags: ["pune", "maharashtra"]
+  },
+  {
     title: "Corona safe",
     link: "https://bit.ly/3v80QOe",
     img: "/assets/images/hospital.png",
@@ -48,12 +120,6 @@ const centralData = [
     tags: ["panindia", "pan india", "pan-india", "india", ""]
   },
   {
-    title: "Covid-19 - Pune",
-    link: "https://covidpune.com/",
-    img: "/assets/images/hospital.png",
-    tags: ["pune", "maharashtra"]
-  },
-  {
     title: "Covid-19 Resources (region-based)",
     link: "https://bit.ly/3dK9DjA",
     img: "/assets/images/hospital.png",
@@ -64,18 +130,6 @@ const centralData = [
     link: "https://bit.ly/3sO2kvB",
     img: "/assets/images/hospital.png",
     tags: ["panindia", "pan india", "pan-india", "india", ""]
-  },
-  {
-    title: "Covid-19 - Beed",
-    link: "https://covidbeed.com/",
-    img: "/assets/images/hospital.png",
-    tags: ["beed", "maharashtra"]
-  },
-  {
-    title: "Covid-19 Resources - Hyderabad",
-    link: "https://hydcovidresources.com/",
-    img: "/assets/images/hospital.png",
-    tags: ["hyd","hyderabad", "telangana"]
   },
   {
     title: "Verified resources database - Covid fighters",
@@ -108,7 +162,7 @@ const centralData = [
     tags: ["panindia", "pan india", "pan-india", "india", ""]
   },
   {
-    title: "Covid19-twitter-search",
+    title: "Covid19-Twitter-search",
     link: "https://covid19-twitter.in/",
     img: "/assets/images/hospital.png",
     tags: ["panindia", "pan india", "pan-india", "india", ""]
@@ -150,30 +204,6 @@ const centralData = [
     tags: ["panindia", "pan india", "pan-india", "india", ""]
   },
   {
-    title: "Covid-19 - Gandhinagar",
-    link: "https://covidgandhinagar.com/",
-    img: "/assets/images/hospital.png",
-    tags: ["gandhinagar", "gandhi nagar", "gandhi-nagar", "gujarat", "guj"]
-  },
-  {
-    title: "Covid-19 - Jamshedpur",
-    link: "https://jamshedpurguru.in/coveasy/",
-    img: "/assets/images/hospital.png",
-    tags: ["jamshedpur", "jamshed pur", "jamshed-pur", "jamshed", "jharkhand"]
-  },
-  {
-    title: "Covid-19 - Nashik",
-    link: "https://covidnashik.com/",
-    img: "/assets/images/hospital.png",
-    tags: ["nashik", "nasik", "maharashtra"]
-  },
-  {
-    title: "Covid-19 - TamilNadu",
-    link: "https://covidtnadu.com/",
-    img: "/assets/images/hospital.png",
-    tags: ["tamilnadu", "tamil nadu", "tamil-nadu", "tamil", "tn"]
-  },
-  {
     title: "Covid tools",
     link: "https://covidtools.in/",
     img: "/assets/images/hospital.png",
@@ -192,30 +222,6 @@ const centralData = [
     tags: ["panindia", "pan india", "pan-india", "india", ""]
   },
   {
-    title: "Stop corona - TamilNadu",
-    link: "https://stopcorona.tn.gov.in/",
-    img: "/assets/images/hospital.png",
-    tags: ["tamilnadu", "tamil nadu", "tamil-nadu", "tn", "tamil"]
-  },
-  {
-    title: "Covidbangalore.support (Instagram)",
-    link: "https://www.instagram.com/covidbangalore.support/",
-    img: "/assets/images/hospital.png",
-    tags: ["karnataka", "karnatak", "bangalore", "bengaluru"]
-  },
-  {
-    title: "Covid-19 resources - Bangalore",
-    link: "https://oxygenblr.in/",
-    img: "/assets/images/hospital.png",
-    tags: ["karnataka", "karnatak", "bangalore", "bengaluru"]
-  },
-  {
-    title: "Covid-19 resources - Kolkata",
-    link: "https://docs.google.com/spreadsheets/u/0/d/1dy1DYjGb15bijab4vVAUyrdtw14va32NgWJqlo_2rcU/htmlview",
-    img: "/assets/images/hospital.png",
-    tags: ["west bengal", "west-bengal", "westbengal", "bengal", "kolkata", "calcutta"]
-  },
-  {
     title: "Covid-19 - RTBot",
     link: "https://twitter.com/covidrtbot",
     img: "/assets/images/hospital.png",
@@ -226,12 +232,6 @@ const centralData = [
     link: "https://covidresources.vercel.app/",
     img: "/assets/images/hospital.png",
     tags: ["panindia", "pan india", "pan-india", "india", ""]
-  },
-  {
-    title: "Pune Covid - links",
-    link: "https://linktr.ee/punecovid",
-    img: "/assets/images/hospital.png",
-    tags: ["pune", "maharashtra"]
   },
   {
     title: "ICLU - India compiled list",
