bcb821b5f1c3aef75a8f9a69f6ca7af1a9949d07
Shivam Singh <57267960+1502shivam-singh@users.noreply.github.com>
2021-05-04 19:34:21 +0530

    Added med objects in medsData

--- src/Data/infoMed.js
+++ src/Data/infoMed.js
@@ -17,37 +17,7 @@ const medsData = [
       img: "/assets/images/oxygen.png",
       tags: ["lucknow", "uttar pradesh", "uttarpradesh", "up", "uttar-pradesh"]
     },
-    {
-      title: "Remdesivir Distributors (pdf) - major states",
-      link: "https://drive.google.com/file/d/1GG4GLuXzz_H9Q-gnEPkGEPeWQDdvrEDY/view",
-      img: "/assets/images/meds.png",
-    tags: [""]
-    },
-    {
-      title: "Ready to fight - Covid",
-      link: "https://readytofightcovid.in/",
-      img: "/assets/images/meds.png",
-    tags: [""]
-    },
-    {
-      title: "Covid Pune - Remdesivir ",
-      link: "https://covidpune.com/remdesivir",
-      img: "/assets/images/meds.png",
-    tags: ["pune", "maharashtra"]
-    },
-    {
-      title: "Remdesivir distributors - cipla",
-      link: "https://www.cipla.com/sites/default/files/Remdesivir-Distributor-List.pdf",
-      img: "/assets/images/meds.png",
-    tags: [""]
-    },
-    {
-      title: "Tocilizumab distributors - cipla",
-      link: "https://www.cipla.com/sites/default/files/Tocilizumab-Distributor-List.pdf",
-      img: "/assets/images/meds.png",
-    tags: [""]
-    },
-    {
+        {
       title: "Remdesivir & Tocilizumab - Lucknow",
       link: "https://external.sprinklr.com/insights/explorer/dashboard/601b9e214c7a6b689d76f493/tab/12?id=DASHBOARD_601b9e214c7a6b689d76f493&widgetId=6085200c039ec95a0fda5477",
       img: "/assets/images/meds.png",
@@ -78,16 +48,70 @@ const medsData = [
     tags: ["maharashtra", "pune", "beed", "ahmednagar", "aurangabad", "solapur", "satara", "nanded", "nashik", "nasik"]
     },
     {
-      title: "Oxygen - Chandigarh LinkedIn post",
-      link: "https://www.linkedin.com/posts/simarpreethsingh_oxygen-community-peoplefirst-activity-6792322902432731136-aAkY",
-      img: "/assets/images/oxygen.png",
-    tags: ["chandigarh", "punjab", "haryana"]
+      title: "Covid Pune - Remdesivir ",
+      link: "https://covidpune.com/remdesivir",
+      img: "/assets/images/meds.png",
+    tags: ["pune", "maharashtra"]
+    },
+    {
+      title: "Fabiflu Medicines - Patna",
+      link: "https://external.sprinklr.com/insights/explorer/dashboard/601b9e214c7a6b689d76f493/tab/48?id=DASHBOARD_601b9e214c7a6b689d76f493&tabId=48%2448_Medicines&widgetId=608c49c7011fb174c4e8cca9",
+      img: "/assets/images/meds.png",
+      tags: ["patna", "bihar"]
+    },
+    {
+      title: "Fabiflu Medicines - Delhi",
+      link: "https://external.sprinklr.com/insights/explorer/dashboard/601b9e214c7a6b689d76f493/tab/1?id=DASHBOARD_601b9e214c7a6b689d76f493&tabId=1%241_Medicines&widgetId=608c0ed0b7cd1c7c449c90dd",
+      img: "/assets/images/meds.png",
+      tags: ["delhi", "ncr", "delhincr", "delhi-ncr"]
+    },
+    {
+      title: "Favipiravir Medicines - Delhi",
+      link: "https://external.sprinklr.com/insights/explorer/dashboard/601b9e214c7a6b689d76f493/tab/1?id=DASHBOARD_601b9e214c7a6b689d76f493&tabId=1%241_Medicines&widgetId=608c0effb7cd1c7c449ca2d5",
+      img: "/assets/images/meds.png",
+      tags: ["delhi", "ncr", "delhincr", "delhi-ncr"]
+    },
+    {
+      title: "Fabiflu Medicines - Lucknow",
+      link: "https://external.sprinklr.com/insights/explorer/dashboard/601b9e214c7a6b689d76f493/tab/12?id=DASHBOARD_601b9e214c7a6b689d76f493&tabId=12%2412_Medicines&widgetId=608c46dd011fb174c4e7e2d2",
+      img: "/assets/images/meds.png",
+      tags: ["delhi", "ncr", "delhincr", "delhi-ncr"]
+    },
+    {
+      title: "Remdesivir Distributors (pdf) - major states",
+      link: "https://drive.google.com/file/d/1GG4GLuXzz_H9Q-gnEPkGEPeWQDdvrEDY/view",
+      img: "/assets/images/meds.png",
+    tags: [""]
+    },
+    {
+      title: "Ready to fight - Covid",
+      link: "https://readytofightcovid.in/",
+      img: "/assets/images/meds.png",
+    tags: [""]
+    },
+    {
+      title: "Remdesivir distributors - cipla",
+      link: "https://www.cipla.com/sites/default/files/Remdesivir-Distributor-List.pdf",
+      img: "/assets/images/meds.png",
+    tags: [""]
+    },
+    {
+      title: "Tocilizumab distributors - cipla",
+      link: "https://www.cipla.com/sites/default/files/Tocilizumab-Distributor-List.pdf",
+      img: "/assets/images/meds.png",
+    tags: [""]
     },
     {
       title: "Ambulance service - Get help now",
       link: "https://www.gethelpnow.in/#/",
       img: "/assets/images/ambulance.png",
     tags: ["mumbai", "bomabay", "delhi", "maharashtra", "bangalore", "bengaluru", "karnataka", "karnatak", "pune"]
+    },
+    {
+      title: "Oxygen - Chandigarh LinkedIn post",
+      link: "https://www.linkedin.com/posts/simarpreethsingh_oxygen-community-peoplefirst-activity-6792322902432731136-aAkY",
+      img: "/assets/images/oxygen.png",
+    tags: ["chandigarh", "punjab", "haryana"]
     }
   ];
     
