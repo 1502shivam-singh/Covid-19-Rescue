babdd1d38bd61cd2e7faf17a41f5335250985209
Jatin <53038248+kaushikjatin@users.noreply.github.com>
2021-05-10 16:43:26 +0530

    1 jatin (#3)
    * Added automatic location search feature
    
    * Added automatic location search
    
    * Added automatic location search
    
    * Used block notations
    
    Co-authored-by: Jatin kaushik <jatinkaushik@Jatins-MacBook-Air.local>

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import medsData from "./Data/infoMed";
 import gsap from "gsap/gsap-core";
 import {ReactComponent as Search} from "./Assets/svg/search.svg";
 import { FiGithub, FiTwitter } from 'react-icons/fi';
-
+import NodeGeocoder from 'node-geocoder'
 export default function App() {
   const headStyle = {
     color: "#c1c1c1"
@@ -83,6 +83,47 @@ export default function App() {
       });
     }
   });
+
+  function success(pos)  {
+    let crd = pos.coords;
+    const latitude=crd.latitude;
+    const longitude=crd.longitude;
+    let apikey = 'USE YOUR API KEY';
+    let api_url = 'https://api.opencagedata.com/geocode/v1/json'
+    let request_url = api_url
+      + '?'
+      + 'key=' + apikey
+      + '&q=' + encodeURIComponent(latitude + ',' + longitude)
+      + '&pretty=1'
+      + '&no_annotations=1';
+    let request = new XMLHttpRequest();
+    request.open('GET', request_url, true);
+    request.onload = function() {
+      if (request.status === 200){ 
+        let data = JSON.parse(request.responseText);
+        const arr=data.results[0].formatted.split(',')
+        let location=arr[arr.length - 2]
+        inputRef.current.value=location;
+        const searchCondition = it => (it.tags.includes(location.toLowerCase())) || it.tags.includes("")
+        setCentralData(centralData.filter(searchCondition));
+        setMealData(mealData.filter(searchCondition));
+        setBedsData(bedsData.filter(searchCondition));
+        setPlasmaData(plasmaData.filter(searchCondition));
+        setMedsData(medsData.filter(searchCondition));
+      } 
+    };
+    request.onerror = function() {};
+    request.send(); 
+  }
+
+  function error(err) {
+    console.log(`ERROR(${err.code}): ${err.message}`);
+  }
+
+  useEffect(()=>{
+      navigator.geolocation.getCurrentPosition(success,error);
+  },[])
+
 
   function setLists(e){
     e.preventDefault();
