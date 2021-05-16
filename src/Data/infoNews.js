a5b0a6d968a5190a0570845d3629ffbc98d030b7
Shivam Singh <57267960+1502shivam-singh@users.noreply.github.com>
2021-05-17 01:56:05 +0530

    Add tip objects to newsData array

--- src/Data/infoNews.js
+++ src/Data/infoNews.js
@@ -3,25 +3,49 @@ const newsData = [
       title: "MyGov - Covid-19",
       link: "https://www.mygov.in/covid-19/",
       img: "/assets/images/news.png",
-    tags: [""]
+      tags: [""]
     },
     {
       title: "How to wear mask properly - NDTV",
       link: "https://www.youtube.com/watch?v=SeV5YL2UMLs",
       img: "/assets/images/tips.png",
-    tags: [""]
+      tags: [""]
     },
     {
       title: "Covid facilities - helpcare",
       link: "https://drive.google.com/file/d/17xsLQPkAc3AgJQqf5903BxP_Q7ICbjgl/view",
       img: "/assets/images/news.png",
-    tags: [""]
+      tags: [""]
     },
     {
       title: "Twitter-search-advanced",
       link: "https://twitter.com/search-advanced",
       img: "/assets/images/tips.png",
-    tags: [""]
+      tags: [""]
+    },
+    {
+      title: "Breathing techniques to increase oxygen",
+      link: "https://www.youtube.com/watch?v=Zs6LkkfC4Pg",
+      img: "/assets/images/tips.png",
+      tags: [""]
+    },
+    {
+      title: "Immunity boosting diet for Covid",
+      link: "https://www.youtube.com/watch?v=jU9BsoHZuiM",
+      img: "/assets/images/tips.png",
+      tags: [""]
+    },
+    {
+      title: "Diet plan for Covid-19 patients (Hindi)",
+      link: "https://www.youtube.com/watch?v=o3_JItSLGEc",
+      img: "/assets/images/tips.png",
+      tags: [""]
+    },
+    {
+      title: "Covid diet: what should you eat?",
+      link: "https://www.youtube.com/watch?v=4NNpJLj00_4",
+      img: "/assets/images/tips.png",
+      tags: [""]
     }
   ];
     
