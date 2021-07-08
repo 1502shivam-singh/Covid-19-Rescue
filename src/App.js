0dbffbcfbe8a2314dcb75eee38de140a290f5ca3
Shivam Singh <57267960+1502shivam-singh@users.noreply.github.com>
2021-07-09 00:04:51 +0530

    chore: disable location detection due to issue with service times

--- src/App.js
+++ src/App.js
@@ -120,7 +120,7 @@ export default function App() {
   }
 
   useEffect(()=>{
-      navigator.geolocation.getCurrentPosition(success,error);
+      // navigator.geolocation.getCurrentPosition(success,error);
   },[])
 
 
